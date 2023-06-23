bcb8bb3687b781581496636ebeeae47a82e8c7bb
mizh-odoo <mizh@odoo.com>
2023-06-23 10:51:11 -0400

    remove print statement

--- ge02_motorcycle_registry_enhancements/models/stock_picking.py
+++ ge02_motorcycle_registry_enhancements/models/stock_picking.py
@@ -13,7 +13,6 @@ class Picking(models.Model):
         for mrp in mrp_ids:
             mrp_order = self.env['mrp.production'].search_read([('id', '=', mrp)])
             product = self.env['product.template'].search_read([('name', '=', mrp_order[0]['product_id'][1])])
-            print(product)
             if product[0]['detailed_type'] == 'motorcycle':
                 if not self.env['motorcycle.registry'].search_count([('vin', '=', mrp_order[0]['lot_producing_id'][1])]) > 0:
                     self.env['motorcycle.registry'].create({
