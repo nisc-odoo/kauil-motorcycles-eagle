a0effc6ffd015a800f31108b99eea45d1badfd82
nisc-odoo <135640237+nisc-odoo@users.noreply.github.com>
2023-06-23 09:54:33 -0400

    Automatic product naming
    Motorcycle products  automatically  named if make, model, year specified and its a motorcycle product.

---
+++ ge07_automatic_product_naming/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Automatic Product Nmaing",
+    
+    "summary": "Automatically name any motorcycle based on make, model, and year.",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This module is an extension of the motorcycle registry. It changes the behaviour of new motorcycles by naming them according to the make, model, and year
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry"],
+    
+    "data": [
+
+    ],
+    
+    "demo": [
+
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
