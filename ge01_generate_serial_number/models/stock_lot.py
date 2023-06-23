bdc1c8cf7896ab3bd8b8f5d464776e3f6175f328
mizh-odoo <mizh@odoo.com>
2023-06-23 10:04:37 -0400

    fix ge01 to get 6 digit serial

--- ge01_generate_serial_number/models/stock_lot.py
+++ ge01_generate_serial_number/models/stock_lot.py
@@ -9,7 +9,7 @@ class StockLot(models.Model):
         if product.detailed_type == 'motorcycle' and product.tracking != "none":
             if product.make and product.year and product.model:
                 temp_serial = product.make + product.model + str(product.year)[2:] + product.battery_capacity.upper()
-                temp_serial += self.env['ir.sequence'].next_by_code('stock.lot.serial')
+                temp_serial += self.env['ir.sequence'].next_by_code('stock.lot.serial')[1:]
                 return temp_serial
         else:
             return super(company, product)
