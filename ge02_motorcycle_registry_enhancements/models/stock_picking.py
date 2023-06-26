268e130dba2638e98fe0ecd748090b6bcbeb7837
mizh-odoo <mizh@odoo.com>
2023-06-26 10:47:22 -0400

    fix picking again

--- ge02_motorcycle_registry_enhancements/models/stock_picking.py
+++ ge02_motorcycle_registry_enhancements/models/stock_picking.py
@@ -5,18 +5,19 @@ class Picking(models.Model):
 
 
     def button_validate(self):
-        if product['detailed_type'] == 'motorcycle':
-            sale_order = self.sale_id
-            mrp_order = sale_order['mrp_production_ids']
-            product = mrp_order['product_id']
-            
-            if not self.env['motorcycle.registry'].search_count([('vin', '=', mrp_order['lot_producing_id']['name'])]) > 0:
-                print("function called")
-                self.env['motorcycle.registry'].create({
-                    'vin': mrp_order['lot_producing_id']['name'],
-                    'stock_lot_ids': [mrp_order['lot_producing_id'][0]['id']],
-                    'sale_order_id': sale_order.id,
-                })
-        return super(Picking,self).button_validate()
-
         
+		sale_order = self.sale_id
+		if sale_order['mrp_production_ids']:
+			mrp_order = sale_order['mrp_production_ids']
+			product = mrp_order['product_id']
+			if product['detailed_type'] == 'motorcycle':
+				if not self.env['motorcycle.registry'].search_count([('vin', '=', mrp_order['lot_producing_id']['name'])]) > 0:
+					print("function called")
+					self.env['motorcycle.registry'].create({
+						'vin': mrp_order['lot_producing_id']['name'],
+						'stock_lot_ids': [mrp_order['lot_producing_id'][0]['id']],
+						'sale_order_id': sale_order.id,
+					})
+		return super(Picking,self).button_validate()	
+
+	
