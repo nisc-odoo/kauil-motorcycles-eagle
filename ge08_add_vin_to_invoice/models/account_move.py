86e0c5ba544de8458f3028d180627c67765b9a2d
befo <befo@odoo.com>
2023-06-26 11:47:24 -0400

    Fixed issue with default vin not following vin pattern

--- ge08_add_vin_to_invoice/models/account_move.py
+++ ge08_add_vin_to_invoice/models/account_move.py
@@ -10,12 +10,12 @@ class AccountMove(models.Model):
             linked_sale_order = self.env["sale.order"].search_read([("name", "=", record.invoice_origin)])
             mrp_id = linked_sale_order[0]["mrp_production_ids"]
             if (len(mrp_id) == 0):
-                record.vin = "No MRP found for vehicle"
+                record.vin = "KAAA0000000000"
                 return
             linked_mrp = self.env["mrp.production"].search_read([("id", "=", mrp_id[0])])
             print(linked_mrp[0]["lot_producing_id"])
             retrieved_vin = linked_mrp[0]["lot_producing_id"]
             if (retrieved_vin == False):
-                record.vin = "No VIN assigned yet"
+                record.vin = "KAAA0000000000"
             else:
                 record.vin = retrieved_vin[1]
