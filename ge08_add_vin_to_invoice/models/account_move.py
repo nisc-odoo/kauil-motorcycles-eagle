8b0f0a3a23f8bf68a59a1f605518b38a61d011a5
befo <befo@odoo.com>
2023-06-26 11:32:18 -0400

    Full functionality

--- ge08_add_vin_to_invoice/models/account_move.py
+++ ge08_add_vin_to_invoice/models/account_move.py
@@ -7,7 +7,15 @@ class AccountMove(models.Model):
 
     def _compute_vin(self):
         for record in self:
-            if (False):
+            linked_sale_order = self.env["sale.order"].search_read([("name", "=", record.invoice_origin)])
+            mrp_id = linked_sale_order[0]["mrp_production_ids"]
+            if (len(mrp_id) == 0):
+                record.vin = "No MRP found for vehicle"
+                return
+            linked_mrp = self.env["mrp.production"].search_read([("id", "=", mrp_id[0])])
+            print(linked_mrp[0]["lot_producing_id"])
+            retrieved_vin = linked_mrp[0]["lot_producing_id"]
+            if (retrieved_vin == False):
                 record.vin = "No VIN assigned yet"
             else:
-                record.vin = "Computed VIN"
+                record.vin = retrieved_vin[1]
