8d554039227610e20f9bdc36f9ed9ff7756dc7c5
mizh-odoo <mizh@odoo.com>
2023-06-26 10:40:07 -0400

    fix picking

--- ge02_motorcycle_registry_enhancements/models/stock_picking.py
+++ ge02_motorcycle_registry_enhancements/models/stock_picking.py
@@ -5,11 +5,11 @@ class Picking(models.Model):
 
 
     def button_validate(self):
-        sale_order = self.sale_id
-        mrp_order = sale_order['mrp_production_ids']
-        product = mrp_order['product_id']
-        
         if product['detailed_type'] == 'motorcycle':
+            sale_order = self.sale_id
+            mrp_order = sale_order['mrp_production_ids']
+            product = mrp_order['product_id']
+            
             if not self.env['motorcycle.registry'].search_count([('vin', '=', mrp_order['lot_producing_id']['name'])]) > 0:
                 print("function called")
                 self.env['motorcycle.registry'].create({
