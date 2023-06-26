1385a9458faae411d02faa1437d725e937dd4ca2
befo <befo@odoo.com>
2023-06-26 14:45:02 -0400

    end of day changes

--- ge08_add_vin_to_invoice/models/account_move.py
+++ ge08_add_vin_to_invoice/models/account_move.py
@@ -8,6 +8,7 @@ class AccountMove(models.Model):
     def _compute_vin(self):
         for record in self:
             linked_sale_order = self.env["sale.order"].search_read([("name", "=", record.invoice_origin)])
+            print(linked_sale_order)
             mrp_id = linked_sale_order[0]["mrp_production_ids"]
             if (len(mrp_id) == 0):
                 record.vin = "KAAA0000000000"
@@ -19,3 +20,4 @@ class AccountMove(models.Model):
                 record.vin = "KAAA0000000000"
             else:
                 record.vin = retrieved_vin[1]
+                
