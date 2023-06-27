2ced39dd0ed2f8929658ffad4766ea8e8cf2fa16
mizh-odoo <mizh@odoo.com>
2023-06-27 09:29:51 -0400

    for testing in sh

--- ge08_add_vin_to_invoice/models/account_move.py
+++ ge08_add_vin_to_invoice/models/account_move.py
@@ -7,14 +7,14 @@ class AccountMove(models.Model):
 
     def _compute_vin(self):
         for record in self:
+            print("Invoice Origin")
+            print(record.invoice_origin)
             linked_sale_order = self.env["sale.order"].search_read([("name", "=", record.invoice_origin)])
-            print(linked_sale_order)
             mrp_id = linked_sale_order[0]["mrp_production_ids"]
             if (len(mrp_id) == 0):
                 record.vin = "KAAA0000000000"
                 return
             linked_mrp = self.env["mrp.production"].search_read([("id", "=", mrp_id[0])])
-            print(linked_mrp[0]["lot_producing_id"])
             retrieved_vin = linked_mrp[0]["lot_producing_id"]
             if (retrieved_vin == False):
                 record.vin = "KAAA0000000000"
