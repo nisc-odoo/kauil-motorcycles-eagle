c3eb2e92c8a91fc40e02f752a3ebbd43a118ca09
mizh-odoo <mizh@odoo.com>
2023-06-28 09:59:22 -0400

    final commit to finish 10

---
+++ ge10_add_map_view/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Add Map View",
+    
+    "summary": "Adds a map view",
+    
+    "description": """
+    Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Reistration and Ownership of each motorcycled of the brand.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "web_map"],
+    
+    "data": [
+		'views/ge10_views.xml'
+    ],
+    
+    "demo": [
+
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
